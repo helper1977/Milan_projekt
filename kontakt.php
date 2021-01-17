2b59d3c2cdcef8bcbeffdda7493a7b00a97ce363
Helper1977 <77569879+helper1977@users.noreply.github.com>
2021-01-17 18:33:42 +0100

    Update kontakt.php

--- kontakt.php
+++ kontakt.php
@@ -1,4 +1,5 @@
 <html>
+   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 
 <head>
 <link rel="stylesheet" type="text/css" href="dizajn.css">
@@ -14,7 +15,7 @@
 <br><br>
 <p>Milan Bašič</p>
 <br>
-<p>milan.basic@helper.si</p>
+<p>milanbasic@helper.si</p>
 
 </div>
 
