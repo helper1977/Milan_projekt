56f105f1d8f215ee9352792461cbc97dd2a4cfce
Helper1977 <77569879+helper1977@users.noreply.github.com>
2021-01-17 13:56:57 +0100

    Update login-header.php

--- login-header.php
+++ login-header.php
@@ -9,7 +9,7 @@
 <div class="nav-main">  
 <div class="nav-item">
 
-<a href="/milan_projekt/home.php"><img src="slike/logo_helper_small.png" alt="logo" width="100px" height="100px"></img></a>
+<a href="/milan_projekt/home.php"><img src="slike/logo_helper_small.jpg" alt="logo" width="100px" height="100px"></img></a>
 
 </div>
 </div>
